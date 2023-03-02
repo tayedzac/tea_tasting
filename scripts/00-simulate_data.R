4f49d37e649c03ff4add874a78fa9640163c10fc
Jayden Jung <jayden.jung@mail.utoronto.ca>
2023-03-02 11:28:03 -0500

    Add preamble

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates data on subject guessing what cups of tea were made the same
+# Author: Team Patricia
+# Data: March 2, 2023
+# Contact: jayden.jung@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
