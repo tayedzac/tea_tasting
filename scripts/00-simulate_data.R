138ed375ea134558b681082eae2848b335e45266
Jayden Jung <jayden.jung@mail.utoronto.ca>
2023-03-02 11:24:53 -0500

    Fix simulated columns

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,15 +10,17 @@
 
 #### Workspace setup ####
 library(tidyverse)
+set.seed(3)   
 
 #### Simulate data ####
-## Cup number is set to 8
-column_01 <- c(1, 2, 3, 4, 5, 6, 7, 8)
-## Randomized answer between Y or N
-column_02 <- sample(x = c("Y", "N"), size = 8, replace = TRUE)
+## Cup's tea type
+column_01 <- c(rep(x = "m", times = 4), rep(x = "t", times = 4))
+## Which group the cup was sorted into 
+column_02 <- c(rep(x = "Group 1", times = 4), rep(x = "Group 2", times = 4))
+column_02 <- sample(column_03)
 ## Assigning names to columns
-simulated_data <- data.frame("cup_number" = column_01,
-                             "Answer" = column_02)
+simulated_data <- data.frame("cup_type" = column_01,
+                             "group" = column_02)
 
 simulated_data
 
