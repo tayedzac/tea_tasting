1132cbfaef2b9f7c06e3973f073b4a32e294a0b3
Tayedza Chikumbirike <t.chikumbirike@mail.utoronto.ca>
2023-03-02 11:10:02 -0500

    simulation written

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,17 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+## Cup number is set to 8
+column_01 <- c(1, 2, 3, 4, 5, 6, 7, 8)
+## Randomized answer between Y or N
+column_02 <- sample(x = c("Y", "N"), size = 8, replace = TRUE)
+## Assigning names to columns
+simulated_data <- data.frame("cup_number" = column_01,
+                             "Answer" = column_02)
+
+simulated_data
 
 
 
